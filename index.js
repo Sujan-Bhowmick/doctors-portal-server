ddb2d59aa5e57a858ec5717f0d4376d048ea6164
Your Name <imsujan93@gmail.com>
2022-12-07 18:54:06 +0600

    nothing to commit

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport'); 
+
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdvx45j.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
@@ -21,11 +25,51 @@ function veryfyJWT(req, res, next) {
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if(err){
-      return res.status(403).send({message:'Forbidden access'})
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
     }
     req.decoded = decoded;
     next();
+  });
+}
+
+const emailSenderOptions = {
+  auth: {
+    api_key: process.env.EMAIL_SENDER_KEY
+  }
+}
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking) {
+  const { patient, patientName, treatment, date, slot} = booking;
+
+
+  var email = {
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `Your Appoinment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    text: `Your Appoinment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    html: `
+  <div>
+   <p> Hello, ${patientName}, </p>
+   <h3> Your Appointment for ${treatment} is confirmed </h3>
+   <p> Looking forward to see you on ${date} at ${slot} </p>
+   <h3> Our Address </h3>
+   <p> Andor killa Bandorban</p>
+   <p>Bangladesh </p>
+   <a href = "https://web.programming-hero.com/">unsubscribe </a>
+  </div>
+  `
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Message sent: ', info)
+    }
   });
 }
 
@@ -35,42 +79,46 @@ async function run() {
     const serviceCollection = client.db('doctors_portal').collection('service');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
     const userCollection = client.db('doctors_portal').collection('users');
+    const doctorsCollection = client.db('doctors_portal').collection('doctors');
+
+    const veryfyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' })
+      }
+    }
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query);
+      const cursor = serviceCollection.find(query).project({ name: 1 });
       const services = await cursor.toArray();
       res.send(services);
     });
-    
-    app.get('/user', veryfyJWT, async(req, res) => {
+
+    app.get('/user', veryfyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
 
-    app.get('/admin/:email', async(req, res) => {
+    app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({email: email});
+      const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin});
+      res.send({ admin: isAdmin });
     })
 
-    app.put('/user/admin/:email', veryfyJWT, async (req, res) => {
+    app.put('/user/admin/:email', veryfyJWT, veryfyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({email: requester});
-      if(requesterAccount.role === 'admin'){
-        const filter = { email: email };
-        const updatedDoc = {
-          $set: {role: 'admin'},
-        };
-        const result = await userCollection.updateOne(filter, updatedDoc);
-        res.send( result);
-      }
-      else{
-        res.status(403).send({message: 'forbidden'})
-      }
-     
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     })
 
     app.put('/user/:email', async (req, res) => {
@@ -128,14 +176,14 @@ async function run() {
 
     app.get('/booking', veryfyJWT, async (req, res) => {
       const patient = req.query.patient;
-      const decodedEmail= req.decoded.email;
-      if(patient === decodedEmail){
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
         const query = { patient: patient };
-      const bookings = await bookingCollection.find(query).toArray();
-       return res.send(bookings);
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
       }
-      else{
-        return res.status(403).send({message: 'forbidden access'});
+      else {
+        return res.status(403).send({ message: 'forbidden access' });
       }
     })
 
@@ -147,7 +195,27 @@ async function run() {
         return res.send({ success: false, booking: exists })
       }
       const result = bookingCollection.insertOne(booking);
+      console.log('sending email');
+      sendAppointmentEmail(booking);
       return res.send({ success: true, result });
+    })
+
+    app.get('/doctor', veryfyJWT, veryfyAdmin, async (req, res) => {
+      const doctors = await doctorsCollection.find().toArray();
+      res.send(doctors);
+    })
+
+    app.post('/doctor', veryfyJWT, veryfyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorsCollection.insertOne(doctor);
+      res.send(result);
+    });
+
+    app.delete('/doctor/:email', veryfyJWT, veryfyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await doctorsCollection.deleteOne(filter);
+      res.send(result);
     })
 
   }

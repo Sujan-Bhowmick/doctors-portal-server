1dd3b2607127bac586cc198f609a4e90ddd5b741
Your Name <imsujan93@gmail.com>
2022-12-11 09:47:15 +0600

    payment confirmation email outline created

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport'); 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -73,6 +74,39 @@ function sendAppointmentEmail(booking) {
   });
 }
 
+function sendPaymentConfirmationEmail(booking) {
+  const { patient, patientName, treatment, date, slot} = booking;
+
+
+  var email = {
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `We have received your payment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    text: `Your payment for this Appoinment ${treatment} is on ${date} at ${slot} is Confirmed`,
+    html: `
+  <div>
+   <p> Hello, ${patientName}, </p>
+   <h3> Thank you for your payment</h3>
+   <h3 >We have received your payment</h3>
+   <p> Looking forward to see you on ${date} at ${slot} </p>
+   <h3> Our Address </h3>
+   <p> Andor killa Bandorban</p>
+   <p>Bangladesh </p>
+   <a href = "https://web.programming-hero.com/">unsubscribe </a>
+  </div>
+  `
+  };
+
+  emailClient.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Message sent: ', info)
+    }
+  });
+}
+
 async function run() {
   try {
     await client.connect()
@@ -80,6 +114,7 @@ async function run() {
     const bookingCollection = client.db('doctors_portal').collection('bookings');
     const userCollection = client.db('doctors_portal').collection('users');
     const doctorsCollection = client.db('doctors_portal').collection('doctors');
+    const PaymentCollection = client.db('doctors_portal').collection('payments');
 
     const veryfyAdmin = async (req, res, next) => {
       const requester = req.decoded.email;
@@ -91,6 +126,18 @@ async function run() {
         res.status(403).send({ message: 'forbidden' })
       }
     }
+
+    app.post('/create-payment-intent',veryfyJWT, async(req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency : 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -183,8 +230,15 @@ async function run() {
         return res.send(bookings);
       }
       else {
-        return res.status(403).send({ message: 'forbidden access' });
+        return res.status(403).send({ message: 'forbidden access'});
       }
+    });
+
+    app.get('/booking/:id', veryfyJWT, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
     })
 
     app.post('/booking', async (req, res) => {
@@ -198,6 +252,22 @@ async function run() {
       console.log('sending email');
       sendAppointmentEmail(booking);
       return res.send({ success: true, result });
+    });
+
+    app.patch('/booking/:id', veryfyJWT, async(req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true, 
+          transactionId: payment.transactionId
+        }
+      }
+      const result = await PaymentCollection.insertOne(payment);
+      const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(updatedDoc)
+
     })
 
     app.get('/doctor', veryfyJWT, veryfyAdmin, async (req, res) => {
